d167a95f1092e8ba734360adb033336aa4209d59
Mariia Borsuk <you@example.com>
2026-02-15 22:42:37 +0100

    ex02 added

---
+++ ex02/Account.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include "Account.hpp"
+int Account :: 	getNbAccounts( void ){
+	return _nbAccounts;
+
+}
+
+	 int Account	::getTotalAmount( void )
+	 {
+		return _totalAmount;
+	 }
+	 int Account::	getNbDeposits( void )
+	{
+		return _totalNbDeposits;
+	}
+ int Account::	getNbWithdrawals( void )
+	{
+		return _totalNbWithdrawals;
+	}
+ void	Account ::displayAccountsInfos( void ){
+
+	}
